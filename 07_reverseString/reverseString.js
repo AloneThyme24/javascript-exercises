367bf7c6ae70660ce92dfce7f2171666fc01c39d
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-08-15 15:21:53 -0700

    Write out function in .js file to pass 1st test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const inputArray = word.split(''); // splits the string into an array
+    const reversedArray = inputArray.reverse() // reverses the array
+    return reversedArray.join('') // join the array into string note the '' in join() glues the letters back together without a separator
 };
 
 // Do not edit below this line
