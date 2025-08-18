cda172bc051caed166e9e3b33cefbcf682e805d4
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-08-18 15:05:49 -0700

    Write function in .js to pass 1st test in .spec.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let runningTotal = 0;
+    for (let i = start; i <= end; i++) {
+        runningTotal += i;
+    }
+    return runningTotal;
 };
 
 // Do not edit below this line
