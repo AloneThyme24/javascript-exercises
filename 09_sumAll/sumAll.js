7122dc0b7fbfd449adda9d679abb919d85d3ef41
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-08-18 16:02:29 -0700

    Add check so 'ERROR' shows when there's non-ints
    This check was added to the function in the .js file so that the 5th test would pass.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function(start, end) {
     if (start < 0 || end < 0) return 'ERROR';
+    
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
 
     let runningTotal = 0;
     const min = Math.min(start, end);
@@ -8,7 +10,7 @@ const sumAll = function(start, end) {
     for (let i = min; i <= max; i++) {
         runningTotal += i;
     }
-    
+
     return runningTotal;
 };
 
