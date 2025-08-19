d4e528c313dfb53e7f818b65b41587afe801165c
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-08-19 16:54:33 -0700

    Add ability to round to one decimal for F and C
    - The functions had to be edited to account for rounding by using Math.round().
    - Instead of returning C or F after the conversion, the functions return 'rounded'.
    - F and C were given this ability simultaneously and both passed when npm test tempConversion.spec.js was run.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function(fahrenheit) {
   const celsius = (fahrenheit - 32) * 5 / 9
-  return celsius;
+  const rounded = Math.round (celsius * 10) / 10
+  return rounded;
 };
 
 const convertToFahrenheit = function(celsius) {
-  const fahrenheit = (celsius * 9/5) + 32;
-  return fahrenheit;
+  const fahrenheit = (celsius * 9/5) + 32
+   const rounded = Math.round (fahrenheit * 10) / 10
+   return rounded;
 };
 
 // Do not edit below this line
