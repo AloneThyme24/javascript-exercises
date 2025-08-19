c3dabdf6d946cadb548ee0ef4e6bae55cbcb7cf1
Emily <104387597+AloneThyme24@users.noreply.github.com>
2025-08-19 16:30:01 -0700

    Add functions in .js to do the conversions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  const celsius = (farenheit - 32) * 5 / 9
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const farenheit = (celsius * 9/5) + 32;
+  return farenheit;
 };
 
 // Do not edit below this line
